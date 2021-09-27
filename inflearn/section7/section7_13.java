0754692c4d2f181443c6c7e9f34e6964470020a1
jyoungjin <ccd1202@naver.com>
2021-09-27 23:19:22 +0900

    add section 7_14

--- inflearn/section7/section7_13.java
+++ inflearn/section7/section7_13.java
@@ -1,48 +1,53 @@
 package section7;
 
 /*
- * 경로 탐색(인접행렬)-(DFS)
+ * 경로 탐색(인접리스트, ArrayList)-(DFS)
  * 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성하세요.
  * 입력설명: 첫 번째 줄에는 정점의 수 N(1<=N<=20)와 간선의 수 M가 주어진다. 그 다음부터 M줄에 걸쳐 연결정보가 주어진다.
  * 출력설명: 총 가지수를 출력한다.
  *  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class section7_12 {
+public class section7_13 {
 
     static int n, m, answer = 0;
-    static int[][] graph;
+    static ArrayList<ArrayList<Integer>> graph;
     static int[] ch;
 
     public void DFS(int v) {
 
         if (v == n) {
             answer++;
-        }else{
-            for (int i = 1; i < n; i++) {
-                if (graph[v][i] == 1 && ch[i] == 0) {
-                    ch[i] = 1;
-                    DFS(i);
-                    ch[i] = 0;
+        } else {
+            for (int nv : graph.get(v)) {
+                if (ch[nv] == 0) {
+                    ch[nv]=1;
+                    DFS(nv);
+                    ch[nv] = 0;
                 }
             }
         }
+
 
     }
 
     public static void main(String[] args) {
 
-        section7_12 T = new section7_12();
+        section7_13 T = new section7_13();
         Scanner scan = new Scanner(System.in);
         n = scan.nextInt();
         m = scan.nextInt();
-        graph = new int[n + 1][n + 1];
+        graph = new ArrayList<ArrayList<Integer>>();
         ch = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
         for (int i = 0; i < m; i++) {
             int a = scan.nextInt();
             int b = scan.nextInt();
-            graph[a][b] = 1;
+            graph.get(a).add(b);
         }
         ch[1] = 1;
         T.DFS(1);
